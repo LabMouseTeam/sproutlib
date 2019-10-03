bfd6dade8e36f048179aa39e80ab0ef16e504d62
Don A. Bailey <donb@labmou.se>
2019-10-03 15:34:42 -0600

    first release of sproutlib

--- README.md
+++ README.md
@@ -1,2 +1,112 @@
-# sproutlib
-A library for defining, propagating, and spawning processes with Python 3.
+# SproutLib
+
+Release: 1.0.0
+
+SproutLib is a set of python3 classes that enable *infrastructure as code* in
+a new, dynamic microservice model.
+
+Instead of defining configuration files that *describe* how services should
+run, services composed of object oriented classes are instantiated *from*
+configuration data.
+
+This allows for more direct relationship between YAML and executable code
+by directly coupling YAML to variables that control each layer of an object
+oriented microservice.
+
+As a result, SproutLib also serves as a way to safely serialize objects
+across a transport, allowing for a single microservice configuration to be
+spawned across N nodes with a single queue write operation. What's elegant
+about the SproutLib model for spawning similar microservices is that the
+service itself can render itself as a string, which will be valid JSON. This
+enables each microservice to clone its specific configuration instantly
+without additional work.
+
+# Usage
+First, create an object that is based on any object tree you like, but is
+rooted at labmouse.sproutlib.SproutRoot
+
+Next, create SproutSchema objects as subclasses. The SproutSchema classes
+define a namespace that will be used as configuration keys in the corresponding
+YAML or JSON configuration.
+
+```class Foo(SproutRoot):
+    class bar(Schema):
+        required = True
+        strict = True
+    class baz(Schema):
+        type = int
+
+    ...
+```
+
+This class can now be instantiated using a Python dict, YAML configuration
+string, or a JSON string. For example, the following Python dict and YAML
+string contains the same information:
+YAML:
+```bar: 'i am a bar'
+baz: 42
+```
+
+```{'bar': 'i am a bar', 'baz': 42}```
+
+and the above class *Foo* can be instantiated with either one with the same
+result:
+```y = """bar: 'i am a bar'\nbaz: 42"""
+f = Foo(y)
+
+d = {'bar': 'i am a bar', 'baz': 42}
+f = Foo(y)```
+
+## Type
+The 'type' parameter dictates what type of data should be stored at this
+namespace. It can be any Python type or custom Python class.
+
+## Required
+The 'required' parameter dictates whether the namespace is included as
+output even if it were never set. For example, when creating a schema
+to interact with a remote API, you may not want (or need) to set all
+parameters in the API call. However, the API call may require the presence
+of the name. 'required' ensures that the name will always appear when the
+string version of the object is generated, helping guarantee that the
+remote API will never raise an exception because of a missing parameter.
+
+For example, in the above class, if we did:
+```f = Foo()
+    f['baz'] = 127
+    api_call = str(f)```
+
+The following would be sent via JSON to the remote API:
+```{"bar": "","baz": 127}```
+
+This makes it faster and simpler to both generate API schemas and interact
+with the remote endpoint.
+
+## Strict
+The 'strict' keyword ensures that the type of object used in a set on the
+schema namespace adheres to the 'type' parameter. If it does not, a
+SproutStrictTypeException is raised.
+
+## Subtype
+For lists or similar python constructs, where a facet of the primary object
+can be any type, the facet itself may be strictly checked. This is done by
+setting the subtype.
+
+It is also necessary to set the subtype so the get action can interpret the
+data within the facet correctly. This is especially necessary when a custom
+class has been placed within a list, for example, and we're converting JSON
+to actual objects. The 'get' action will convert the name to the appropriate
+type seamlessly and transparently.
+
+## Hidden
+This parameter hides a particular namespace during the string generation
+process. This option is deprecated in favor of simply using a class or
+instance variable that does not inherit the SproutSchema base class.
+
+# Validation
+For validation, Lab Mouse suggests the use of a modern schema validation tool
+such as the @23andMe Yamale utility.
+https://github.com/23andMe/Yamale
+
+This tool can help you define not only the YAML configuration files for your
+project, but how your objects should be defined within your microservice as
+you compose your code.
---
+++ labmouse/sproutlib/Sproutlib.py
@@ -0,0 +1,305 @@
+import inspect
+import json
+import yaml
+
+
+class SproutStrictTypeException(Exception):
+    pass
+
+
+class SproutSchema(object):
+    required = False
+    strict = True
+    hidden = False
+    subtype = None
+    type = str
+
+
+class SproutRoot(dict):
+    __BUILTIN__ = [
+        bytes,
+        float,
+        str,
+        int,
+        bool,
+        dict,
+        tuple,
+        object,
+        ]
+
+    __iter_i = 0
+    __iter_l = []
+
+    def _getmembers(self):
+        x = inspect.getmembers(self.__class__, lambda a: SproutRoot._find(a))
+        return [i[1] for i in x]
+
+    def _find(a):
+        if not inspect.isclass(a):
+            return False
+
+        if issubclass(a, SproutSchema):
+            return True
+
+        return False
+
+    def __init__(self, *args, **kw):
+        # Init the dict with nothing in it.
+        dict.__init__(self, (), **kw)
+
+        # If we were loaded with a dict, process each name by class.
+        if len(args) == 0:
+            return
+
+        # If we were seeded with a string, presume it's either YAML or JSON,
+        # and parse it into an object.
+        if len(args) == 1 and type(args[0]) == str:
+            try:
+                a = json.loads(args[0])
+            except Exception as E:
+                try:
+                    a = yaml.safe_load(args[0])
+                except Exception as E:
+                    raise E
+        else:
+            a = args[0]
+
+        self.__do_update(a)
+
+    def __do_update(self, d):
+        for k in d.keys():
+            # Try and resolve the class. If we can't do so, just fall-back to
+            # the name.
+            c = ''
+            try:
+                c = getattr(self, k)
+            except Exception as e:
+                c = k
+
+            dict.__setitem__(self, c, d[k])
+
+    def keys(self):
+        # Only return keys that exist or are required.
+        r = []
+        l = self._getmembers()
+        for i in l:
+            if ((i not in self) and (i.required is True)) or (i in self):
+                r += [i]
+
+        return r
+
+    def update(self, K):
+        self.__do_update(K)
+
+    def __iter__(self):
+        """
+        This does not return strings, so that the result can be useful to
+        functions that would use classes as indicies.
+        """
+        self.__iter_l = self._getmembers()
+        self.__iter_i = 0
+
+        while self.__iter_i < len(self.__iter_l):
+            k = self.__iter_l[self.__iter_i]
+            self.__iter_i += 1
+            if k.hidden is True:
+                continue
+
+            # Feign an object.
+            if k not in self and k.required is True:
+                yield (k, k.type())
+            elif k in self:
+                yield (k, self[k])
+
+    def __test_strict(self, _v, _t):
+        if not isinstance(_v, _t):
+            raise SproutStrictTypeException("SproutRoot.set: strict ({0}!={1})".format(
+                            _t,
+                            type(_v)))
+
+    def __string_to_schema(self, k):
+        for i in self._getmembers():
+            if i.__name__ == k:
+                return i
+
+        return None
+
+    def __setitem__(self, k, v):
+        if type(k) == str:
+            k = self.__string_to_schema(k)
+
+        if k.strict is True:
+            _t = k.type
+            _v = v
+
+            # We only validate two levels deep.
+            if _t == list and k.subtype is not None:
+                # First, make sure the list is a list.
+                self.__test_strict(v, list)
+
+                # Now, ensure each facet of the list is valid.
+                _t = k.subtype
+                for i in v:
+                    self.__test_strict(i, _t)
+            else:
+                self.__test_strict(_v, _t)
+
+        return dict.__setitem__(self, k, v)
+
+    def __getitem__(self, x):
+        # Handle custom types
+        if ((x in self) and
+           (x.type not in self.__BUILTIN__)):
+            o = None
+            if ((x.type == list) and
+               ((x.subtype != list) and
+               (x.subtype != tuple))):
+
+                l = []
+                for i in dict.__getitem__(self, x):
+                    o = x.subtype
+
+                    # If the object has an update method, use it.
+                    up = getattr(o, 'update', None)
+                    if (up is not None) and (o in self.__BUILTIN__):
+                        o.update(i)
+                    elif (up is not None) and (o not in self.__BUILTIN__):
+                        o = o(i)
+                    else:
+                        o = i
+
+                    l += [o]
+                return l
+
+            elif ((x.type == list) and
+                  ((x.subtype == list) or
+                  (x.subtype == tuple))):
+                return dict.__getitem__(self, x)
+
+            else:
+                o = x.type()
+                o.update(dict.__getitem__(self, x))
+                return o
+
+        elif ((x not in self) and (x.required is True)):
+            return x.type()
+
+        # If the type is a builtin, just return the value
+        return dict.__getitem__(self, x)
+
+    def __repr__(self):
+        # We have to override repr or it will leak keys not in KEYS. And
+        # frankly, I don't care about the single-quote versus double-quote
+        # regression. If we see this as a problem in the future, we'll fix it.
+        return self.__str__()
+
+    def items(self):
+        x = dict.items(self)
+
+        # Don't let items() ruin transitions from normal models to Shorts
+        d = []
+        for i in x:
+            if i[0] not in self:
+                continue
+            d += [i]
+
+        return d
+
+    def __hash__(self):
+        t = tuple(self.items())
+        t2 = []
+        for i in t:
+            D1 = i[0]
+            if type(i[0]) == dict or type(i[0]) == list:
+                D1 = json.dumps(i[0])
+
+            D2 = i[1]
+            if type(i[1]) == dict or type(i[1]) == list:
+                D2 = json.dumps(i[1])
+
+            t2 += [(D1, D2)]
+
+        return hash(tuple(t2))
+
+    def __str__(self):
+        d = {}
+        for i in self:
+            # __iter__ should always return the set of attributes based on the
+            # SproutSchema class.
+            i = i[0]
+
+            # We ignore the required attribute and skip an object if it is
+            # contained within this list. This is to ensure the name doesn't
+            # appear in strings at all; i.e. internal objects only.
+            if i.hidden is True:
+                continue
+
+            # If the type is required then generate a default value regardless
+            # of whether or not it was defined by the writer.
+            q = i.required
+            t = i.type
+
+            if i in self:
+                d[i.__name__] = self[i]
+            elif q is True:
+                d[i.__name__] = t()
+
+        return self.__json_dumps(d)
+
+    def className(self):
+        return self.__class__.__name__
+
+    def __json_dumps(self, d):
+        if isinstance(d, dict):
+            return self.__json_dict(d)
+        elif isinstance(d, list):
+            return self.__json_list(d)
+        elif isinstance(d, tuple):
+            return self.__json_list(d)
+
+    def __json_dict(self, d):
+        s = []
+        for k in d.keys():
+            v = d[k]
+            if type(k) != str and issubclass(k, SproutSchema):
+                k = k.__name__
+
+            if (isinstance(v, list) or
+                    isinstance(v, tuple) or
+                    isinstance(v, dict)):
+                v = self.__json_dumps(v)
+                s += ["\"{0}\": {1}".format(k, v)]
+            else:
+                if isinstance(v, int) or isinstance(v, float):
+                    # Bool is a sub of int. Make json happy.
+                    if isinstance(v, bool):
+                        if v is True:
+                            v = 'true'
+                        else:
+                            v = 'false'
+
+                    s += ["\"{0}\": {1}".format(k, v)]
+                else:
+                    s += ["\"{0}\": \"{1}\"".format(k, v)]
+
+        s = ",".join(s)
+
+        return "{0}{1}{2}".format('{', s, '}')
+
+    def __json_list(self, d):
+        s = []
+        for v in d:
+            if (isinstance(v, list) or
+                    isinstance(v, tuple) or
+                    isinstance(v, dict)):
+                v = self.__json_dumps(v)
+                s += [v]
+            else:
+                if isinstance(v, int) or isinstance(v, float):
+                    s += ["{0}".format(v)]
+                else:
+                    s += ["\"{0}\"".format(v)]
+
+        s = ",".join(s)
+
+        return "[{0}]".format(s)
